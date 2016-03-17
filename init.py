1a066cffe761f649b5063c7aaf5e2dde63bae85b
Reece Small <rsmall@salisbury6c.ac.uk>
2016-03-17 15:45:30 +0000

    Update init.py

--- init.py
+++ init.py
@@ -8,8 +8,8 @@ def setup():
     #gpio.setwarnings(False)
     powerLed = 6
     tempSensor = 8
-    inputs = [tempSensor]   # Set there categories in arrays
-    outputs = [powerLed]
+    inputs = tempSensor   # Set there categories in arrays
+    outputs = powerLed
     print('### ATTEMPTING TO IMPORT AND SETUP PINS ###')
     thepins = pins.Pins(inputs, outputs)    #Set up pins from a class
     print('### ALL PINS IMPORTED AND SETUP SUCCESSFULLY ###')
