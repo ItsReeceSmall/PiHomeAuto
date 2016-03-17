08b48c2ff57d7b1bf4eb614517464f43f261fd02
Reece Small <rsmall@salisbury6c.ac.uk>
2016-03-17 15:44:05 +0000

    Update pins.py

--- pins.py
+++ pins.py
@@ -6,23 +6,7 @@ class Pins:
         self.__inputs = inputs
         self.__outputs = outputs
         self.printPins()
-        
-    @property
-    def Inputs(self):
-        return self.__inputs
-    
-    @Inputs.setter
-    def Inputs(self, value):
-        self.__inputs = value
-        
-    @property
-    def Outputs(self):
-        return self.__outputs
-        
-    @Outputs.setter
-    def Outputs(self, value):
-        self.__outputs = value
-    
+
     # Need to write code to setup all the pins that have been passed in
     
     def printPins(self):
