984dd7184c74e921a4c125d68acc113589908549
Reece Small <rsmall@salisbury6c.ac.uk>
2016-03-17 15:22:43 +0000

    Update init.py

--- init.py
+++ init.py
@@ -3,17 +3,14 @@ import time, os, sys
 import pins
 
 def setup():
-    #set GPIO up
-    gpio.setmode(gpio.BOARD)
+    gpio.setmode(gpio.BOARD)    #set GPIO up
     #gpio.setwarnings(False)
-    #Set up pins from a class
     powerLed = 6
     tempSensor = 8
-    # Set there categories
-    inputs = [tempSensor]
+    inputs = [tempSensor]   # Set there categories in arrays
     outputs = [powerLed]
     print('### ATTEMPTING TO IMPORT AND SETUP PINS ###')
-    thepins = pins.Pins(inputs, outputs)
+    thepins = pins.Pins(inputs, outputs)    #Set up pins from a class
     print('### ALL PINS IMPORTED AND SETUP SUCCESSFULLY ###')
 
 setup()
