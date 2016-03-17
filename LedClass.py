69bc4b18a5de6faa6fa24f14494fdfadb389553d
Reece Small <rsmall@salisbury6c.ac.uk>
2016-03-17 15:21:09 +0000

    Update LedClass.py

--- LedClass.py
+++ LedClass.py
@@ -1,2 +1,26 @@
 import RPi.GPIO as gpio
 import time, sys, os
+
+class Led:
+    def __init__(self, powerLed):
+        self.__powerLed = powerLed
+        #self.powerLedOn()
+        #self.powerLedOff()
+        
+    @property
+    def powerLed(self):
+        return self.__powerLed
+    
+    @powerLed.setter
+    def powerLed(self, value):
+        self.__powerLed = value
+    
+    # Need to write code to setup all the pins that have been passed in
+    
+    def powerLedOn(self):
+        gpio.input(powerLed, HIGH)
+        time.sleep(0.2)
+    
+    def powerLedOff(self):
+        gpio.input(powerLed, LOW)
+        time.sleep(0.2)
