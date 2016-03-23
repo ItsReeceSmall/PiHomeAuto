32ce61d6259b439275859284e2c5a8b55a7d7f1e
Reece Small <rsmall@salisbury6c.ac.uk>
2016-03-23 12:34:57 +0000

    Update pirClass.py

--- pirClass.py
+++ pirClass.py
@@ -1,12 +1,12 @@
 import RPi.GPIO as gpio
 import time, os, sys
 
-GPIO.setmode(GPIO.BCM)
-PIR_PIN = 7
+GPIO.setmode(GPIO.BOARD)
+PIR_PIN = 32
 GPIO.setup(PIR_PIN, GPIO.IN)
 
 def MOTION(PIR_PIN):
-               print “Motion Detected!”
+               print (“Motion Detected!”)
 
 print “PIR Module Test (CTRL+C to exit)”
 time.sleep(2)
