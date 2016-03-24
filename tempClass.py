a50973e13bc90c86bb39b8e80a90a15ae47dc389
Reece Small <rsmall@salisbury6c.ac.uk>
2016-03-24 10:16:21 +0000

    Update tempClass.py

--- tempClass.py
+++ tempClass.py
@@ -9,7 +9,8 @@ class Temp:
         base_dir = '/sys/bus/w1/devices/'
         device_folder = glob.glob(base_dir + '28*')[0]
         device_file = device_folder + '/w1_slave'
-        self.tempBoth()
+        self.read_temp_c()
+        self.read_temp_f()
 
     def read_temp_raw():
         f = open(device_file, 'r')
@@ -17,7 +18,7 @@ class Temp:
         f.close()
         return lines
 
-    def read_temp_C():
+    def read_temp_C(self):
         lines = read_temp_raw()
         while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
@@ -26,9 +27,9 @@ class Temp:
         if equals_pos != -1:
             temp_string = lines[1][equals_pos+2:]
             temp_c = float(temp_string) / 1000.0
-            return temp_c
+           #return temp_c
 
-    def read_temp_f():
+    def read_temp_f(self):
         lines = read_temp_raw()
         while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
@@ -38,8 +39,4 @@ class Temp:
             temp_string = lines[1][equals_pos+2:]
             temp_c = float(temp_string) / 1000.0
             temp_f = temp_c * 9.0 / 5.0 + 32.0
-            return temp_f
-    
-    def tempBoth(self):
-        read_temp_c()
-        read_temp_f()
+            #return temp_f
