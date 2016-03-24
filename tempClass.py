cc9ef41fa0f6767ee2c0044f4112af287a62d977
Reece Small <rsmall@salisbury6c.ac.uk>
2016-03-24 15:40:54 +0000

    Update tempClass.py

--- tempClass.py
+++ tempClass.py
@@ -2,15 +2,23 @@ import os
 import glob
 import time
 
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
 class Temp:
     def __init__(self):
-        os.system('modprobe w1-gpio')
-        os.system('modprobe w1-therm')
-        base_dir = '/sys/bus/w1/devices/'
-        device_folder = glob.glob(base_dir + '28*')[0]
-        device_file = device_folder + '/w1_slave'
-        self.read_temp_c()
-        self.read_temp_f()
+        pass
+    
+    @property
+    def C(self):
+        return self.read_temp()[0]
+    
+    @property
+    def F(self):
+        return self.read_temp()[1]
 
     def read_temp_raw():
         f = open(device_file, 'r')
@@ -18,18 +26,7 @@ class Temp:
         f.close()
         return lines
 
-    def read_temp_C(self):
-        lines = read_temp_raw()
-        while lines[0].strip()[-3:] != 'YES':
-            time.sleep(0.2)
-            lines = read_temp_raw()
-        equals_pos = lines[1].find('t=')
-        if equals_pos != -1:
-            temp_string = lines[1][equals_pos+2:]
-            temp_c = float(temp_string) / 1000.0
-           #return temp_c
-
-    def read_temp_f(self):
+    def read_temp(self):
         lines = read_temp_raw()
         while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
@@ -39,4 +36,4 @@ class Temp:
             temp_string = lines[1][equals_pos+2:]
             temp_c = float(temp_string) / 1000.0
             temp_f = temp_c * 9.0 / 5.0 + 32.0
-            #return temp_f
+            return temp_c, temp_f
