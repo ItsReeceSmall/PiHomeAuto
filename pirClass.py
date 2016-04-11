31c353f5b543969426b918cbb1ae2a0c6d6fec7a
Reece Small <rsmall@salisbury6c.ac.uk>
2016-04-11 17:29:54 +0100

    Update pirClass.py

--- pirClass.py
+++ pirClass.py
@@ -4,13 +4,14 @@ import time, os, sys
 class Pir:
     def __init__(self, pirSensor):
         self.pirSensor = pirSensor
+        self.pirState = 0
         self.pirMethod()
     
     def pirMethod(self):
-        pirState = gpio.input(self.pirSensor)
-        if pirState == 1:
+        self.pirState = gpio.input(self.pirSensor)
+        if self.pirState == 1:
             pirVal = 'on'
-        elif pirState == 0:
+        elif self.pirState == 0:
             pirVal = 'off'
         else:
             print('Error PIR not functioning, aborting...')
