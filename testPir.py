27c3c68f9cd352d873ea04ca0715db356ab1c19e
Reece Small <rsmall@salisbury6c.ac.uk>
2016-04-11 18:49:45 +0100

    Update testPir.py

--- testPir.py
+++ testPir.py
@@ -6,16 +6,16 @@ PIR_PIN = 32
 GPIO.setup(PIR_PIN, GPIO.IN)
 
 def MOTION(PIR_PIN):
-               print “Motion Detected!”
+  print “Motion Detected!”
 
 print “PIR Module Test (CTRL+C to exit)”
 time.sleep(2)
 print “Ready”
 
 try:
-               GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
-               while 1:
-                              time.sleep(100)
+  GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
+  while 1:
+    time.sleep(100)
 except KeyboardInterrupt:
-               print “ Quit”
-               GPIO.cleanup()
+  print “ Quit”
+  GPIO.cleanup()
