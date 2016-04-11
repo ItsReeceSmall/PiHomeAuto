4e72eccfd9fb715bfb276969193ba890a824be6a
Reece Small <rsmall@salisbury6c.ac.uk>
2016-04-11 20:42:24 +0100

    Update distClass.py

--- distClass.py
+++ distClass.py
@@ -1,36 +1,33 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio
 import time, os, sys
 
-GPIO.setmode(GPIO.BOARD)
+class Dist:
+  def __init__(self, dtSensor, deSensor):
+      self.dtSensor = dtSensor
+      self.deSensor = deSensor
+      self.distCheck()
+  
+  def distCheck():
+      gpio.setup(self.dtSensor,gpio.OUT)
+      gpio.setup(self.deSensor,gpio.IN)
+      gpio.output(self.dtSensor, False)
+      time.sleep(1)
+      gpio.output(self.dtSensor, True)
+      time.sleep(0.00001)
+      gpio.output(self.dtSensor, False)
+      while gpio.input(self.deSensor)==0:
+          pulse_start = time.time()
+      while gpio.input(self.deSensor)==1:
+          pulse_end = time.time()
+      pulse_duration = pulse_end - pulse_start
+      distance = pulse_duration * 17150
+      distance = round(distance, 2)
+      dist = str(distance)
+      return dist
 
-TRIG = 36
-ECHO = 38
-
-print ("Distance Measurement In Progress")
-
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
-
-GPIO.output(TRIG, False)
-print ("Waiting For Sensor To Settle")
-time.sleep(2)
-
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
-
-while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
-
-while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
-
-pulse_duration = pulse_end - pulse_start
-
-distance = pulse_duration * 17150
-
-distance = round(distance, 2)
-
-print ("Distance: "  + str(distance) + " cm")
-
-GPIO.cleanup()
+if __name__ == "__main__":
+    gpio.setmode(gpio.BOARD)
+    dtSensor = 36
+    deSensor = 38
+    d = Dist()
+    d(dtSensor, deSensor)
