f351d0cfb1af4bbb92d2f29ceda0e137950c5ecb
Reece Small <rsmall@salisbury6c.ac.uk>
2016-04-12 18:03:04 +0100

    Update progressBar.py

--- progressBar.py
+++ progressBar.py
@@ -5,3 +5,17 @@ import lcd1602 as LCD1602
 lcd = LCD1602()
 
 class Bar:
+    def __init__(self):
+        self.barMethod()
+    
+    def barMethod(self):
+        val = 1
+        for i in range(1, 101):
+            val = (val + 1)
+            print (str(val))
+            time.sleep(0.1)
+            
+
+if __name__ == "__main__":
+    Bar()
+    sys.exit()
