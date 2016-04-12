b9819a36f7e1332e08e1b25c725f69f02ad16138
Reece Small <rsmall@salisbury6c.ac.uk>
2016-04-12 13:55:36 +0100

    Update pins.py

--- pins.py
+++ pins.py
@@ -1,5 +1,8 @@
 import RPi.GPIO as gpio
 import time, os, sys
+from lcd1602 import LCD1602
+
+lcd = LCD1602()
 
 class Pins:
     def __init__(self, inputs, outputs):
@@ -12,7 +15,11 @@ class Pins:
     def printPins(self):
         for pin in self.__inputs:
             gpio.setup(pin, gpio.IN)
+            lcd.lcd_string('Pin ' + pin + ' setup', lcd.LCD_LINE_1)
             print ('### Pin ' + str(pin) + ' is setup')
+            time.sleep(0.15)
         for pin in self.__outputs:
             gpio.setup(pin, gpio.OUT)
+            lcd.lcd_string('Pin ' + pin + ' setup', lcd.LCD_LINE_1)
             print ('### Pin ' + str(pin) + ' is setup')
+            time.sleep(0.15)
