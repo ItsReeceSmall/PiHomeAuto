a581637a96f3342866668d328c7737aafbcbca09
Reece Small <rsmall@salisbury6c.ac.uk>
2016-04-12 18:19:37 +0100

    Update progressBar.py

--- progressBar.py
+++ progressBar.py
@@ -10,10 +10,24 @@ class Bar:
     
     def barMethod(self):
         val = -1
+        #mult = 0
         for i in range(1, 102):
             val = (val + 1)
-            lcd.lcd_string('Progress    ' + str(val) + '%', lcd.LCD_LINE_2)
-            time.sleep(0.1)
+            #mult = (mult + 0.0618811881188119)
+            if val > 6.25:
+                lcd.lcd_string('█', lcd.LCD_LINE_1)
+                if val > 12.5:
+                    lcd.lcd_string('██', lcd.LCD_LINE_1)
+                    if val > 18.75:
+                        lcd.lcd_string('███', lcd.LCD_LINE_1)
+                        if val >= 25:
+                            lcd.lcd_string('████', lcd.LCD_LINE_1)
+                            if val > 31.25:
+                                lcd.lcd_string('█████', lcd.LCD_LINE_1)
+                                if val > 37.5:
+                                    lcd.lcd_string('██████', lcd.LCD_LINE_1)
+            lcd.lcd_string('PROGRESS    ' + str(val) + '%', lcd.LCD_LINE_2)
+            time.sleep(0.06)
             
 
 if __name__ == "__main__":
