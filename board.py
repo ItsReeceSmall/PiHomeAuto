073a907550a04f1aba2798bd2bee034ba8ca5471
Reece Small <rsmall@salisbury6c.ac.uk>
2016-04-15 12:14:30 +0100

    Update board.py

--- board.py
+++ board.py
@@ -7,10 +7,14 @@ class Board:
         self.setup()
     
     def setup(self):
+        self.cleanup()
         #print ('Warnings = False')
         self.board.setwarnings(False)
         #print ('Board = gpio.BOARD')
         self.board.setmode(self.board.BOARD)
+    
+    def cleanup(self):
+        self.board.cleanup()
 
 if __name__ == "__main__":
     b = Board()
