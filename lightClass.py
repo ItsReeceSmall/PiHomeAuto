700da6de6ccbe07697fa332cffde9092adb11401
Reece Small <rsmall@salisbury6c.ac.uk>
2016-04-19 18:55:37 +0100

    buzz as 32 for night time testing, testing new ui, testing dist physical reconfig

--- lightClass.py
+++ lightClass.py
@@ -6,17 +6,17 @@ board = Board().board
 class Light:
     def __init__(self, lightSensor, board):
         self.board = board
-        self.lightSensor = 18
+        self.lightSensor = lightSensor
         self.lightMethod()
 
     def lightMethod(self):
-        self.board.setup(lightSensor, self.board.OUT)
-        self.board.output(lightSensor, self.board.LOW)
+        self.board.setup(self.lightSensor, self.board.OUT)
+        self.board.output(self.lightSensor, self.board.LOW)
         measurement = 0
         time.sleep(0.05)
-        self.board.setup(lightSensor,self.board.IN)
+        self.board.setup(self.lightSensor,self.board.IN)
         #for i in range(1,11):
-        if self.board.input(lightSensor) == self.board.LOW:
+        if self.board.input(self.lightSensor) == self.board.LOW:
             measurement += 1
         return measurement
 
