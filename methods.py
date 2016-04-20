b8ff57dfe22a93d4a3b1166b67907d75cd4533c0
ItsReeceSmall <rsmall@salisbury6c.ac.uk>
2016-04-20 12:18:23 +0100

    added light sens, removed dist for time being

--- methods.py
+++ methods.py
@@ -45,7 +45,9 @@ def getPir(pirSensor, board, counter, pirLight, buzzSensor):
     if value == 1:
         finValue = 'ON '
         l(pirLight, board).LedOn()
+        print ('counter was: ' + str(counter))
         counter = (counter + 1)
+        print ('counter now is: ' + str(counter))
         b(buzzSensor, board).buzzOn()
         time.sleep(0.4)
         b(buzzSensor, board).buzzOff()
