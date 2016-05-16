76613b2f907732097f6a5cf954b8ee85d57acd46
Reece Small <rsmall@salisbury6c.ac.uk>
2016-05-17 00:14:40 +0100

    about changes to layout for m

--- main.py
+++ main.py
@@ -93,14 +93,10 @@ def vls():
     VLS = request.forms.get('buttonState')
     print('DEBUG: VLS state = ' + str(VLS))
     print('DEBUG: Brightness Slider = ' + str(brightness))
-    on = bool(int(VLS))
-    print('DEBUG: buttonState = ' + str(on))
     light = board.PWM(M.fadeLed, 100)
     light.start(100)
-    if on:
-        time.sleep(0.2)
+    if VLS == '1':
         light.ChangeDutyCycle(int(brightness))
-        time.sleep(3)
     else:
         light.ChangeDutyCycle(0)
 
