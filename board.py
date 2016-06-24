9405147e49b9675480eb5d1e1e247f156131f144
Reece Small <rsmall@salisbury6c.ac.uk>
2016-06-24 14:25:06 +0100

    Update board.py

--- board.py
+++ board.py
@@ -5,15 +5,18 @@ class Board:
     def __init__(self):
         self.board = gpio
         self.setup()
+        # sets up board
     
     def setup(self):
         self.board.setwarnings(False)
         self.board.setmode(self.board.BOARD)
+        # sets up what mode the board is
     
     def cleanup(self):
         print('### User input recieved ###\n### Closing down ###')
         self.board.cleanup()
         sys.exit()
+        # cleanup method to end the program safely
 
 if __name__ == "__main__":
     b = Board()
